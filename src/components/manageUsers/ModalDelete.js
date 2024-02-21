ff164d951ee621a480441ce1eb93cd79d31b630f
quangkhai482002 <118819349+quangkhai482002@users.noreply.github.com>
2024-02-22 00:29:23 +0700

    CRUD-delete

--- src/components/manageUsers/ModalDelete.js
+++ src/components/manageUsers/ModalDelete.js
@@ -2,19 +2,44 @@ import React, { useEffect, useState } from "react";
 import { Modal } from "antd";
 
 const ModalDelete = (props) => {
-  return (
-    <>
-      <Modal
-        title="Confirm delete user"
-        open={props.open}
-        onOk={props.onOk}
-        onCancel={props.onCancel}
-      >
-        <p>
-          Are you sure you want to delete <bold>{props.dataModal.email}</bold> ?
-        </p>
-      </Modal>
-    </>
-  );
+  const showDeleteConfirm = () => {
+    Modal.confirm({
+      title: "Confirm delete user",
+      content: `Are you sure you want to delete user?`,
+      okText: "Yes",
+      okType: "danger",
+      cancelText: "No",
+      onOk() {
+        props.onOk();
+      },
+      onCancel() {
+        props.onCancel();
+      },
+    });
+  };
+
+  // Call the function when the component is rendered
+  React.useEffect(() => {
+    if (props.open) {
+      showDeleteConfirm();
+    }
+  }, [props.open]);
+
+  return null;
+  // return (
+  //   <>
+  //     <Modal
+  //       title="Confirm delete user"
+  //       open={props.open}
+  //       onOk={props.onOk}
+  //       onCancel={props.onCancel}
+  //     >
+  //       <p>
+  //         Are you sure you want to delete user:{" "}
+  //         <strong>{props.dataModal.email}</strong> ?
+  //       </p>
+  //     </Modal>
+  //   </>
+  // );
 };
 export default ModalDelete;
